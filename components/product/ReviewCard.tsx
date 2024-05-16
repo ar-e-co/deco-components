3307273c6bad94b6a5296df51b54863504d14554
Diego <diego.o.guimaraes29@gmail.com>
2024-05-16 14:19:13 -0300

    fix: styled anatomy

--- components/product/ReviewCard.tsx
+++ components/product/ReviewCard.tsx
@@ -3,9 +3,10 @@ import { AnatomyClasses, handleClasses } from "../../sdk/styles.ts";
 
 const anatomy = [
   "container",
-  "title",
+  "author",
   "stars",
-  "opinionsText",
+  "date",
+  "opinionText",
 ] as const;
 
 type Styles = AnatomyClasses<typeof anatomy[number]>;
@@ -30,63 +31,63 @@ function ReviewCard({ ratingValue,reviewBody,classes,datePublished,author }: Pro
       };
       
   return (
-        <div class="flex flex-col w-full lg:w-1/5 lg:max-w-[290px] border-t border-black py-5 px-3 lg:h-44 text-14 font-body ">
+        <div class={handleClasses(classes?.container) || "flex flex-col w-full lg:w-1/5 lg:max-w-[290px] border-t border-black py-5 px-3 lg:h-44 text-14 font-body "}>
             <div class="rating  rating-sm">
                 {ratingValue == 1
                     ? (
                     <input
                         type="radio"
                         name="rating-5"
-                        class="mask mask-star"
+                        class={handleClasses(classes?.stars) || "mask mask-star"}
                         checked
                     />
                     )
-                    : <input type="radio" name="rating-5" class="mask mask-star" />}
+                    : <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} />}
                 {ratingValue == 2
                     ? (
                     <input
                         type="radio"
                         name="rating-5"
-                        class="mask mask-star"
+                        class={handleClasses(classes?.stars) || "mask mask-star"}
                         checked
                     />
                     )
-                    : <input type="radio" name="rating-5" class="mask mask-star" />}
+                    : <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} />}
                 {ratingValue == 3
                     ? (
                     <input
                         type="radio"
                         name="rating-5"
-                        class="mask mask-star"
+                        class={handleClasses(classes?.stars) || "mask mask-star"}
                         checked
                     />
                     )
-                    : <input type="radio" name="rating-5" class="mask mask-star" />}
+                    : <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"}/>}
                 {ratingValue == 4
                     ? (
                     <input
                         type="radio"
                         name="rating-5"
-                        class="mask mask-star"
+                        class={handleClasses(classes?.stars) || "mask mask-star"}
                         checked
                     />
                     )
-                    : <input type="radio" name="rating-5" class="mask mask-star" />}
+                    : <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} />}
                 {ratingValue == 5
                     ? (
                     <input
                         type="radio"
                         name="rating-5"
-                        class="mask mask-star"
+                        class={handleClasses(classes?.stars) || "mask mask-star"}
                         checked
                     />
                     )
-                    : <input type="radio" name="rating-5" class="mask mask-star" />}
+                    : <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} />}
             </div>
-                <span class="mt-1 lg:min-h-24 ">{reviewBody}</span>
+                <span class={handleClasses(classes?.opinionText) || "mt-1 lg:min-h-24 "}>{reviewBody}</span>
             <div class="flex flex-col w-full text-12 mt-3 font-body">
-                <span>{author}</span>
-                <span>Compra feita {dataFormatada(datePublished)}</span>
+                <span class={handleClasses(classes?.author)}>{author}</span>
+                <span class={handleClasses(classes?.date)}>Compra feita {dataFormatada(datePublished)}</span>
             </div>
         </div>
   );
