27f1cef2c353e65f98be056d212b58b48ba00c0c
Pedro Dalla Bernardina <l.pedro.nascimento@gmail.com>
2024-05-20 16:21:18 -0300

    chore: remove unused vars

--- sdk/getProductSpecification.ts
+++ sdk/getProductSpecification.ts
@@ -1,6 +1,9 @@
-import type { Product , ProductLeaf,PropertyValue } from "apps/commerce/types.ts";
+import type { Product, PropertyValue } from "apps/commerce/types.ts";
 
-export const getProductSpecification = (specification :String , isVariantOf: Product["isVariantOf"]) => {
-  const specificationItem = isVariantOf?.additionalProperty.filter((p: PropertyValue ) => specification?.includes(p?.name as string) );
+export const getProductSpecification = (specification: string, isVariantOf: Product["isVariantOf"]) => {
+  const specificationItem = isVariantOf
+    ?.additionalProperty
+    .filter((p: PropertyValue ) => specification?.includes(p?.name as string) );
+
   return specificationItem;
 };
