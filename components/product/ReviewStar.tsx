28871d52ff509d84e1b862092fdd9d7922767129
Pedro Dalla Bernardina <l.pedro.nascimento@gmail.com>
2024-05-20 17:50:25 -0300

    lint

--- components/product/ReviewStar.tsx
+++ components/product/ReviewStar.tsx
@@ -1,5 +1,4 @@
-
-import { AnatomyClasses, handleClasses } from "../../sdk/styles.ts";
+import { AnatomyClasses, handleClasses } from "deco-components/sdk/styles.ts";
 
 const anatomy = [
   "container",
@@ -14,27 +13,31 @@ export interface Props {
     title?:string ; 
     reviewCount: number;
     ratingValue: number;
-    opinionsText: string;
     classes?:Styles;
 }
 
-
-function ReviewStar({ ratingValue,reviewCount,title="Avaliações",opinionsText="opniões",classes }: Props) {
+function ReviewStar({ 
+  ratingValue, 
+  reviewCount, 
+  classes,
+  title = "Avaliações", 
+}: Props) {
   return (
     <div class={handleClasses(classes?.container) || "flex flex-col"}>
-            <span class={handleClasses(classes?.title) || "text-14 font-body mt-5 mb-4"}>{title}</span>
-            <div class="flex flex-row">
-              <div class="rating">
-                {ratingValue == 1 ? ( <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked/>):(<input type="radio" name="rating-5" class="mask mask-star"/>)}
-                {ratingValue == 2 ? ( <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked/>):(<input type="radio" name="rating-5" class="mask mask-star"/>)}
-                {ratingValue == 3 ? ( <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked/>):(<input type="radio" name="rating-5" class="mask mask-star"/>)}
-                {ratingValue == 4 ? ( <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked/>):(<input type="radio" name="rating-5" class="mask mask-star"/>)}
-                {ratingValue == 5 ? ( <input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked/>):(<input type="radio" name="rating-5" class="mask mask-star"/>)}
+      <span class={handleClasses(classes?.title) || "text-14 font-body mt-5 mb-4"}>{title}</span>
 
-              </div>
-              <span class={handleClasses(classes?.opinionsText) || "text-12 font-body ml-3"}>{reviewCount} opiniões</span>
-            </div>
+      <div class="flex flex-row">
+        <div class="rating">
+          {ratingValue == 1 ? (<input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked />) : (<input type="radio" name="rating-5" class="mask mask-star" />)}
+          {ratingValue == 2 ? (<input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked />) : (<input type="radio" name="rating-5" class="mask mask-star" />)}
+          {ratingValue == 3 ? (<input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked />) : (<input type="radio" name="rating-5" class="mask mask-star" />)}
+          {ratingValue == 4 ? (<input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked />) : (<input type="radio" name="rating-5" class="mask mask-star" />)}
+          {ratingValue == 5 ? (<input type="radio" name="rating-5" class={handleClasses(classes?.stars) || "mask mask-star"} checked />) : (<input type="radio" name="rating-5" class="mask mask-star" />)}
+        </div>
+
+        <span class={handleClasses(classes?.opinionsText) || "text-12 font-body ml-3"}>{reviewCount} opiniões</span>
       </div>
+    </div>
   );
 }
 
