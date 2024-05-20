5ffef025566d45496a4897ddf1a58e4388dde015
Pedro Dalla Bernardina <l.pedro.nascimento@gmail.com>
2024-05-20 16:25:33 -0300

    chore: move analytics to deco-components

--- sdk/useAddToCart.ts
+++ sdk/useAddToCart.ts
@@ -1,28 +1,42 @@
-import { sendEvent } from "../../simples/sdk/analytics.tsx";
+import { useState } from "preact/hooks";
 import { AddToCartParams } from "apps/commerce/types.ts";
+import { sendEvent } from "./analytics.tsx";
 
 export interface Props {
-    eventParams: AddToCartParams;
-    onAddItem: () => Promise<void>; 
-  }
+  eventParams: AddToCartParams;
+  onAddItem: () => Promise<void>;
+}
 
 export const useAddToCart = ({ eventParams, onAddItem, }: Props) => {
-    const onClick = async (e: MouseEvent) => {
-      e.preventDefault();
-      e.stopPropagation();
-  
-      try {
-  
-        await onAddItem();
-  
-        sendEvent({
-          name: "add_to_cart",
-          params: eventParams,
-        });
-  
-      } finally {
-      }
-    };
-  
-    return { onClick, "data-deco": "add-to-cart" };
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const addItems = async (e: MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    try {
+      setLoading(true);
+      await onAddItem();
+
+      sendEvent({
+        name: "add_to_cart",
+        params: eventParams,
+      });
+
+    }
+    catch(err) {
+      setError(err);
+    }
+    finally {
+      setLoading(false);
+    }
   };
+
+  return { 
+    addItems, 
+    loading,
+    error,
+    "data-deco": "add-to-cart", 
+  };
+};
