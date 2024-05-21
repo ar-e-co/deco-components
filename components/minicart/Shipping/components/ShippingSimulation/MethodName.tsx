9fb9160455739f7b25571dc7c2aafa5c401785fa
Diego <diego.o.guimaraes29@gmail.com>
2024-05-20 22:42:36 -0300

    Anatomy style component

---
+++ components/minicart/Shipping/components/ShippingSimulation/MethodName.tsx
@@ -0,0 +1,29 @@
+import { AnatomyClasses, handleClasses } from "../../../../../sdk/styles.ts";
+
+const anatomy = [
+  "container",
+] as const;
+
+export type EstimateInfoStyle = AnatomyClasses<typeof anatomy[number]>;
+
+export type ShippingOptionEstimateProps = {
+  method: DisplaySla
+  classes?: EstimateInfoStyle;
+};
+  type DisplaySla = {
+    name: string;
+    shippingEstimate: string;
+    price: number;
+    storeNames?: string[];
+    isPickup?: boolean;
+  };
+  
+ export default function MethodName({ method, classes }: ShippingOptionEstimateProps) {
+    return (
+      <span class={handleClasses(classes?.container) || "text-button text-start lg:text-center"}>
+        {method.name}
+      </span>
+    );
+  }
+
+  
