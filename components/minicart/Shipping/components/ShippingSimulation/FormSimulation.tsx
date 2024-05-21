f5554ec4bd74e275a68dc1643fe5e7a27e78d2b6
Diego <diego.o.guimaraes29@gmail.com>
2024-05-21 00:23:29 -0300

    add handleClasses

--- components/minicart/Shipping/components/ShippingSimulation/FormSimulation.tsx
+++ components/minicart/Shipping/components/ShippingSimulation/FormSimulation.tsx
@@ -2,14 +2,23 @@ import { useCart } from "apps/vtex/hooks/useCart.ts";
 import { useSignal,SKU } from "@preact/signals";
 import { useCallback } from "preact/hooks";
 import { useShippingCalculator } from "../../sdk/useShippingCalculator.ts";
+import { AnatomyClasses, handleClasses } from "../../../../../sdk/styles.ts";
 
-
-export interface Props {
-    items: Array<SKU>;
-  }
-
+  const anatomy = [
+    "container",
+    "input",
+    "button"
+  ] as const;
   
-function FormSimulation({ items }: Props) {
+  type Styles = AnatomyClasses<typeof anatomy[number]>;
+  
+  export type ClearSelectedShippingButtonProps = {
+    classes?: Styles;
+    items: Array<SKU>;
+    textButton?: string
+  };
+
+function FormSimulation({ items,classes, textButton }: Props) {
     const { postalCode , simulateResult } = useShippingCalculator();
     const loading = useSignal(false);
     const { simulate, cart } = useCart();
@@ -17,7 +26,6 @@ function FormSimulation({ items }: Props) {
       if (postalCode.value.length !== 8) {
         return;
       }
-  
       try {
         loading.value = true;
         simulateResult.value = await simulate({
@@ -30,22 +38,18 @@ function FormSimulation({ items }: Props) {
       }
     }, [items, postalCode.value]);
 
-  
-    console.log(simulateResult)
-    return (
-     
-  
+      return (
         <form
           onSubmit={(e) => {
             e.preventDefault();
             handleSimulation();
           }}
-          class="w-full flex flex-row"
+          class={handleClasses( classes?.container) || "w-full flex flex-row"}
         >
           <input
             as="input"
             type="text"
-            class=" bg-gray-100 rounded-full px-4  join-item w-full border border-transparent outline-none"
+            class={handleClasses( classes?.input) || " bg-gray-100 rounded-full px-4  join-item w-full border border-transparent outline-none"}
             placeholder="Seu cep aqui"
             value={postalCode.value}
             maxLength={8}
@@ -56,12 +60,11 @@ function FormSimulation({ items }: Props) {
           />
           <button
             type="submit"
-            class="btn no-animation btn-primary join-item ml-2 "
+            class={handleClasses( classes?.button) || "btn no-animation btn-primary join-item ml-2 "}
           >
-            Calcular
+            {textButton ||"Calcular"}
           </button>
         </form>
-  
     );
   }
   
