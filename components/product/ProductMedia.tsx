0eec27f66da88404b8f4cb0e3c0a581a784954a5
Diego <diego.o.guimaraes29@gmail.com>
2024-05-22 21:51:07 -0300

    fix: add handleClasses

--- components/product/ProductMedia.tsx
+++ components/product/ProductMedia.tsx
@@ -2,75 +2,100 @@ import Image from "apps/website/components/Image.tsx";
 import ProductImageZoom from "../../../simples/islands/ProductImageZoom.tsx";
 import imgZoom from "deco-components/components/product/ImgZoom.tsx";
 import ProductVideoMedia from "./ProductVideoMedia.tsx";
-export interface Props {
-    url:string;
-    alt:string;
-    actionOnClick?: "zoom" | "modal";
-    onMouseOver?: boolean;
-    width: number;
-    height:  number;
-    index: number
-}
+import { AnatomyClasses, handleClasses } from "../../sdk/styles.ts";
 
-export default function ProductMedia({alt,url,index,actionOnClick="zoom",onMouseOver=false,height,width}: Props) {
+
+const anatomy = [
+  "container",
+  "image",
+  "video"
+] as const;
+
+type StylesImage = AnatomyClasses<typeof anatomy[number]>;
+
+
+export type StylesImageProps = {
+  classes?: StylesImage;
+  url:string;
+  alt:string;
+  actionOnClick?: "zoom" | "modal";
+  onMouseOver?: boolean;
+  width: number;
+  height:  number;
+  index: number
+};
+
+
+
+export default function ProductMedia({alt,url,index,actionOnClick="zoom",onMouseOver=false,height,width,classes}: StylesImageProps) {
     const aspectRatio = `${width} / ${height}`;
 
   function addID(id) {
     imgZoom(id);
   }
-  
 
   return (
     <>
-        <div
-        index={index}
-        id={`box${index}`}
-        class={`items-center justify-center m-0 lg:min-h-[820px]  overflow-hidden relative  ${actionOnClick == "zoom" && "cursor-zoom-in"}`}
-        onClick={(e) => (actionOnClick == "zoom" && addID(e.target.id))}
-        >
-          {(() => {
-          const extension = url.split('.').pop();
-          if (extension === 'mp4' || extension === 'mp3') {
-            return (
-              <ProductVideoMedia 
-                  alt={alt}
-                  width={width}
-                  height={height}
-                  index={index}                  
-                  url={url}/>
-            );
-          } else {
-            return ( 
-            <Image
-              class={`bg-base-100 col-span-full row-span-full rounded w-full h-full opacity-100 lg:group-hover:opacity-0`}
-              sizes="(max-width: 640px) 100vw, 40vw"
-              style={{aspectRatio}}
-              src={url}
-              alt={alt}
-              width={width}
-              height={height}
-              preload={index === 0}
-              loading={index === 0 ? "eager" : "lazy"}
-              id={index}
-              name={index}
-              />
-             );
-          }
-        })()}
-
-            
-        </div>
-
-        {actionOnClick == "modal" &&
-        (
-            <div class="absolute top-2 right-2 bg-transparent w-full h-full">
-            <ProductImageZoom
-                images={sliderImages}
-                width={700}
-                height={Math.trunc(700 * height / width)}
+      <div
+      index={index}
+      id={`box${index}`}
+      class={handleClasses(classes?.container) + `${actionOnClick == "zoom" && "cursor-zoom-in"} `}
+      onClick={(e) => (actionOnClick == "zoom" && addID(e.target.id))}
+      >
+        {(() => {
+        const extension = url.split('.').pop();
+        if (extension === 'mp4' || extension === 'mp3') {
+          return (
+            <ProductVideoMedia 
+                alt={alt}
+                width={width}
+                height={height}
+                index={index}                  
+                url={url}
+                classes={{video: classes?.video}}
+                />
+          );
+        } else {
+          return ( 
+          <><Image
+            class={handleClasses(classes?.image)}
+            sizes="(max-width: 640px) 100vw, 40vw"
+            style={{aspectRatio}}
+            src={url}
+            alt={alt}
+            width={width}
+            height={height}
+            preload={index === 0}
+            loading={index === 0 ? "eager" : "lazy"}
+            id={index}
+            name={index}
             />
-            </div>
-        )}
+             <ProductVideoMedia 
+                alt={alt}
+                width={width}
+                height={height}
+                index={index}                  
+                url={url}
+                classes={{video: classes?.video}}
+                />
+            </>
+            );
+        }
+      })()}
+
+          
+      </div>
+
+      {actionOnClick == "modal" &&
+      (
+          <div class="absolute top-2 right-2 bg-transparent w-full h-full">
+          <ProductImageZoom
+              images={sliderImages}
+              width={700}
+              height={Math.trunc(700 * height / width)}
+          />
+          </div>
+      )}
     </>
   );
 }
--- components/product/ProductVideoMedia.tsx
+++ components/product/ProductVideoMedia.tsx
@@ -1,28 +1,40 @@
-export interface Props {
-    url:string;
-    alt:string;
-    index: number
-    width: number;
-    height:  number;
-}
+import { AnatomyClasses, handleClasses } from "../../sdk/styles.ts";
 
-export default function ProductVideoMedia({alt,url,index,height,width}: Props) {
+const anatomy = [
+  "video"
+] as const;
+
+type StylesVideo = AnatomyClasses<typeof anatomy[number]>;
+
+
+export type StylesVideoProps = {
+  classes?: StylesVideo;
+  url:string;
+  alt:string;
+  index: number
+  width: number;
+  height:  number;
+};
+
+export default function ProductVideoMedia({alt,url,index,height,width,classes}: StylesVideoProps) {
   return (  
     <>
-      <video className="w-full h-auto" autoPlay
-          alt={alt}
-          width={width}
-          height={height}
-          preload={index === 0}
-          loading={index === 0 ? "eager" : "lazy"}
-          id={index}
-          name={index}                  
-          webkit-playsinline
-          muted
-          loop
-          x5-playsinline
-          playsInline
-          src={url}>
+      <video 
+        class={handleClasses(classes?.video)}
+        autoPlay
+        alt={alt}
+        width={width}
+        height={height}
+        preload={index === 0}
+        loading={index === 0 ? "eager" : "lazy"}
+        id={index}
+        name={index}                  
+        webkit-playsinline
+        muted
+        loop
+        x5-playsinline
+        playsInline
+        src={url}>
       </video>
     </>
   );
