8beb694e3b87714d43e9a75932aff5099e6bf13e
Pedro Dalla Bernardina <l.pedro.nascimento@gmail.com>
2024-05-31 09:31:38 -0300

    fix: styles

--- components/ui/Avatar.tsx
+++ components/ui/Avatar.tsx
@@ -18,18 +18,26 @@ export type AvatarVariants = "active" | "disabled" | "default" | "none";
 export interface Props {
   variant?: AvatarVariants;
   classes?: AvatarStyles;
-  children: ComponentChildren
+  children: ComponentChildren;
 }
-
 
 function Avatar({ variant = "none", classes, children }: Props) {
   const variants = {
-    active: handleClasses("bg-primary [&]:text-base-200 [&]:ring-primary", classes?.active),
-    disabled: handleClasses("[&]:hover:ring-base-200 cursor-default after:absolute after:top-1/2 after:h-0.5 after:-left-[5%] after:bg-primary after:w-[110%] after:block after:rotate-[30deg] after:opacity-30 after:content-['']", classes?.disabled),
-    none: ""
+    active: handleClasses(
+      "bg-primary [&]:text-base-200 [&]:ring-primary",
+      classes?.active,
+    ),
+    disabled: handleClasses(
+      "[&]:hover:ring-base-200 cursor-default after:absolute after:top-1/2 after:h-0.5 after:-left-[5%] after:bg-primary after:w-[110%] after:block after:rotate-[30deg] after:opacity-30 after:content-['']",
+      classes?.disabled,
+    ),
+    none: "",
   };
 
-  const containerClasses = handleClasses("relative transition-all text-base-content ring-1 ring-base-200 hover:ring-base-content  rounded-full flex items-center justify-center w-13 lg:w-12 h-8 lg:h-7", classes?.container);
+  const containerClasses = handleClasses(
+    "relative transition-all text-base-content ring-1 ring-base-200 hover:ring-base-content  rounded-full flex items-center justify-center w-13 lg:w-12 h-8 lg:h-7",
+    classes?.container,
+  );
   const variantClasses = variants[variant as keyof typeof variants];
 
   return (
