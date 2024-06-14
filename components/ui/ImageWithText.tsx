c01e629a7d43e0f4d1c328a2ee090f0f7b7571b4
Pedro Dalla Bernardina <pedro.bernardina@guidance.dev>
2024-06-14 08:06:28 -0300

    feat: further modularize

--- components/ui/ImageWithText.tsx
+++ components/ui/ImageWithText.tsx
@@ -1,79 +1,67 @@
+import { ComponentChildren } from "preact";
 import DecoImage, {
   Props as ImageProps,
 } from "apps/website/components/Image.tsx";
-import Icon from "deco-components/components/ui/Icon.tsx";
-import { AnatomyClasses, handleClasses } from "../../sdk/styles.ts";
+
+import { AnatomyClasses, handleClasses } from "deco-components/sdk/styles.ts";
 
 const anatomy = [
   "container",
-  "link",
   "image",
-  "cardTitle",
-  "cardText",
-  "cardAfter",
+  "content",
 ] as const;
 
 type ImageWithTextClasses = AnatomyClasses<typeof anatomy[number]>;
 
-interface Props extends Omit<ImageProps, "alt"> {
-  cardTitle: string;
+interface Props extends ImageProps {
+  children?: ComponentChildren;
   classes?: ImageWithTextClasses;
   width: number;
-  height: number;
-  link?: string;
+  height?: number;
+  href?: string;
 }
 
 function ImageWithText({
+  classes,
+  href,
+  children,
   width,
   height,
-  classes,
-  cardTitle,
-  link,
-  src,
+  ...props
 }: Props) {
+  let aspectRatio;
+  let paddingTop;
+
+  if (height) {
+    aspectRatio = width / height;
+    paddingTop = `${parseFloat((1 / aspectRatio).toFixed(2)) * 100}%`;
+  }
+
   return (
     <a
-      href={link}
-      class={handleClasses("group relative", classes?.container)}
+      href={href}
+      class={handleClasses("group relative block", classes?.container)}
+      {...paddingTop && { style: { paddingTop, height: 0 } }}
     >
       <DecoImage
-        src={src}
-        width={width}
-        height={height}
-        alt={cardTitle}
-        class={handleClasses("w-full h-full object-cover", classes?.image)}
+        class={handleClasses(
+          "absolute top-0 left-0 block w-full h-full object-cover",
+          classes?.image,
+        )}
         loading="lazy"
+        height={height}
+        width={width}
+        {...props}
       />
-      {cardTitle && (
-        <div
-          class={handleClasses(
-            "absolute w-fit ml-4 mb-3.5 bottom-0 left-0 right-0 ",
-            classes?.cardTitle,
-          )}
-        >
-          <div
-            class={handleClasses(
-              "flex w-auto font-medium leading-tight text-gray-1000 items-center",
-              classes?.cardText,
-            )}
-          >
-            {cardTitle}
-            <Icon
-              class="h-auto ml-1.5 transition-all ease-in-out duration-250 group-hover:rotate-45"
-              id="ArrowPointingOut"
-              width={12}
-              height={12}
-            />
-          </div>
-          <div
-            class={handleClasses(
-              "after:content-[''] after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-0.5 after:bg-gray-1000 after:transition-all after:duration-200 after:ease-in-out group-hover:after:left-0 group-hover:after:w-full",
-              classes?.cardAfter,
-            )}
-          >
-          </div>
-        </div>
-      )}
+
+      <div
+        class={handleClasses(
+          "absolute top-0 left-0 w-full h-full flex font-medium text-gray-1000 items-center",
+          classes?.content,
+        )}
+      >
+        {children}
+      </div>
     </a>
   );
 }
