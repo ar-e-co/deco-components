24c9821e53e7ccea4642be8fd5b64cfe2e692364
Pedro Dalla Bernardina <pedro.bernardina@guidance.dev>
2024-07-01 11:19:03 -0300

    fix: types

--- components/ui/HeaderBar/HeaderBarModal.tsx
+++ components/ui/HeaderBar/HeaderBarModal.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useRef, useState } from "preact/hooks";
 
 function HeaderBarModal() {
-  const [state, setState] = useState(0)
+  const [state, setState] = useState(0);
 
-  return (
-    <button onClick={() => setState(c => c + 1)}>count: {state}</button>
-  )
+  return <button onClick={() => setState((c) => c + 1)}>count: {state}</button>;
 }
 
-export default HeaderBarModal
+export default HeaderBarModal;
