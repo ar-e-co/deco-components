69fa482ca754cef095c57a9218dfdab5931489f7
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2024-07-01 20:20:57 -0300

    create phonelink component

---
+++ components/ui/PhoneLink.tsx
@@ -0,0 +1,25 @@
+function PhoneLink({ phone, anchorClass }: { phone: string, anchorClass?: string }) {
+  
+    let phoneFormatted = phone.replace(/\D/g, '');
+  
+    if (phoneFormatted.startsWith('55')) {
+      phoneFormatted = `+${phoneFormatted}`
+    } else if (!phoneFormatted.startsWith('+55')) {
+      phoneFormatted = `+55${phoneFormatted}`
+    }
+  
+    if (phoneFormatted[3] === '0') {
+      phoneFormatted = phoneFormatted.replace('0', '')
+    }
+    
+    return (
+      <a
+        class={anchorClass}
+        href={`tel:${phoneFormatted}`}
+      >
+        {phone}
+      </a>
+    );
+  }
+  
+  export default PhoneLink;
