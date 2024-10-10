cf7838c6e95a5afc9462c8d8ccee2f493e778a42
Pedro Dalla Bernardina <pedro.bernardina@guidance.dev>
2024-10-10 19:22:24 -0300

    Revert "chore: udpate files"
    This reverts commit 8e538719e9d84709a13b32ec8b748ee2ac8e715d.

--- mod.ts
+++ mod.ts
@@ -1,20 +1,27 @@
+import type { App as A, AppContext as AC } from "deco/mod.ts";
 import type { Secret } from "apps/website/loaders/secret.ts";
 import manifest, { Manifest } from "./manifest.gen.ts";
-import { type App as A, type AppContext as AC } from "@deco/deco";
+
 export interface ConfigVerifiedReviews {
   idWebsite: string;
   secretKey?: Secret;
   plateforme?: string;
 }
+
 export interface Props {
   configVerifiedReviews: ConfigVerifiedReviews;
 }
+
 /**
  * @title AR&Co Components
  * @description Configs to be used by AR&Co Components
  * @category arco
  */
-export default function MyApp(state: Props): A<Manifest, Props> {
+export default function MyApp(state: Props): A<
+  Manifest,
+  Props
+> {
   return { state, manifest };
 }
+
 export type AppContext = AC<ReturnType<typeof MyApp>>;
